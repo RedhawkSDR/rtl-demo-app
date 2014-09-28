959de35d55b1fe114a346a811534f99a409e62da
Douglas Pew <douglas.pew@axiosengineering.com>
2014-09-28 19:00:05 -0400

    When building the local SDR 1) Sets the host for the GPP device 2) Only warns about missing optional components

--- bin/copysdr.sh
+++ bin/copysdr.sh
@@ -2,19 +2,44 @@
 mydir=`dirname $0`
 mydir=`cd "$mydir" && pwd`
 TMPDIR="$mydir/tmp"
+export SRCSDR=${SDRROOT:-/var/redhawk/sdr}
+DESTSDR="${mydir}/sdr"
 
 err() {
    echo "ERROR: $@"  1>&2
    exit 1
 }
 
-export SYSSDRROOT=/var/redhawk/sdr
-SDRROOT="${mydir}/sdr"
+warn() {
+   echo "WARN: $@" 1>&2
+}
 
-if [ ! -e "${SYSSDRROOT}/$1" ] ; then
-    err "$SYSSDRROOT/$1 does not exists"
+alias errwarn=err
+
+usage() {
+    cat <<EOFEOF
+Usage: $0 [-wh] [-s SDRROOT]
+
+    -h             Show help
+    -s SDRROOT     Use the given SDRROOT.  Defaults to $SRCSDR
+    -w             Warn (do not fail) if asset does not exists
+EOFEOF
+}
+
+while getopts "hs:w" opt ; do
+    case "$opt" in
+        s) SRCSDR=$OPTARG ;;
+        h) usage; exit 0 ;;
+        w) alias errwarn=warn ;;
+        *) err "BAD PARAMETER $opt" ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ ! -e "${SRCSDR}/$1" ] ; then
+    errwarn "$SRCSDR/$1 does not exist" && exit 0
 fi
 
-destdir=`dirname "$SDRROOT/$1"` || err
+destdir=`dirname "$DESTSDR/$1"` || err
 /bin/mkdir -p "$destdir" || err "Unable to create directory $destdir"
-/bin/cp -L -r "${SYSSDRROOT}/$1" "$destdir" || err "Unable to copy $SYSSDRROOT/$1"
+/bin/cp -L -r "${SRCSDR}/$1" "$destdir" || err "Unable to copy $SRCSDR/$1"
--- bin/mksdr.sh
+++ bin/mksdr.sh
@@ -1,22 +1,33 @@
 #!/bin/sh -ex
 ./copysdr.sh /dev/mgr
 ./copysdr.sh /dom/mgr
+./copysdr.sh /dom/domain
+
+# GPP Node
 ./copysdr.sh /dev/devices/GPP
+./copysdr.sh /dev/nodes/DevMgr_${HOSTNAME}
+
+# RTL Front End device
 ./copysdr.sh /dev/devices/RTL2832U
-./copysdr.sh /dev/devices/sim_RX_DIGITIZER
-./copysdr.sh /dev/nodes/DevMgr_${HOST}
 ./copysdr.sh /dev/nodes/RTL2832_Node
-./copysdr.sh /dev/nodes/sim_RX_DIGITIZER_Node
+
+# RTL FM Waveform
+./copysdr.sh /dom/waveforms/RTL_FM_Waveform
+
+# FM Components
+./copysdr.sh /dom/components/RTL_FM_Controller
 ./copysdr.sh /dom/components/agc
 ./copysdr.sh /dom/components/psd
 ./copysdr.sh /dom/components/AmFmPmBasebandDemod
 ./copysdr.sh /dom/components/DataConverter
 ./copysdr.sh /dom/components/NOOP
-./copysdr.sh /dom/components/RTL_FM_Controller
 ./copysdr.sh /dom/components/ScaleOutput
 ./copysdr.sh /dom/components/TuneFilterDecimate
+
+# Softpackage dependencies
 ./copysdr.sh /dom/deps/dsp
 ./copysdr.sh /dom/deps/fftlib
-./copysdr.sh /dom/domain
-./copysdr.sh /dom/waveforms/RTL_FM_Waveform
 
+# Optional components
+./copysdr.sh -w /dev/devices/sim_RX_DIGITIZER
+./copysdr.sh -w /dev/nodes/sim_RX_DIGITIZER_Node
