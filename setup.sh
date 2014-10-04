1c87ba3a5eba4bf299bc571805c86c5ec3750af2
Rob Cannon <rob.cannon@axiosengineering.com>
2014-10-04 13:51:06 -0400

    Added COPYRIGHT Headers

--- setup.sh
+++ setup.sh
@@ -1,4 +1,23 @@
 #!/bin/sh -e
+#
+# This file is protected by Copyright. Please refer to the COPYRIGHT file
+# distributed with this source distribution.
+#
+# This file is part of REDHAWK rtl-demo-app.
+#
+# REDHAWK rtl-demo-app is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# REDHAWK rtl-demo-app is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+#
 
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f "$0")
