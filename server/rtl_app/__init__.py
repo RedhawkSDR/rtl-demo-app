ebd539efe37c7d3bd41529b137aea198baf21433
Douglas Pew <douglas.pew@axiosengineering.com>
2014-10-04 18:58:39 -0400

    Added copyright headers to reorganized files

--- server/rtl_app/__init__.py
+++ server/rtl_app/__init__.py
@@ -1,3 +1,22 @@
+#
+# This file is protected by Copyright. Please refer to the COPYRIGHT file
+# distributed with this source distribution.
+#
+# This file is part of REDHAWK server.
+#
+# REDHAWK server is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# REDHAWK server is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+#
 from _common import BadDemodException, BadFrequencyException, DeviceUnavailableException
 from rtl_app import RTLApp, AsyncRTLApp
 from mock_rtl_app import AsyncRTLApp as MockAsyncRTLApp, RTLApp as MockRTLApp
