835d51f79261a0514681ddd4000f45deea1a6380
Rob Cannon <rob.cannon@axiosengineering.com>
2014-10-08 16:41:24 -0400

    Moved the base url so that development and production urls are the same

--- server/server.py
+++ server/server.py
@@ -34,26 +34,34 @@ from devices import RTL2832U, sim_RX_DIGITIZER
 from rest import SurveyHandler, DeviceHandler, EventHandler, PSDHandler
 from rtl_app import AsyncRTLApp
 
-# establish static directory from this module
-staticdir = os.path.join(os.path.dirname(__import__(__name__).__file__), 'static')
+# establish static directory from this module as one up from current
+static_dir = os.path.abspath(os.path.join(os.path.dirname(__import__(__name__).__file__), '..', 'static'))
+_BASE_URL = r'/rtl'
 
 
 def get_application(rtl_app, _ioloop=None, debug=False):
-    application = tornado.web.Application([
-        (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": staticdir}),
-        (r"/survey", SurveyHandler, dict(rtl_app=rtl_app, ioloop=_ioloop)),
-        (r"/device", DeviceHandler, dict(rtl_app=rtl_app, ioloop=_ioloop)),
-        (r"/status", EventHandler, dict(rtl_app=rtl_app, _ioloop=_ioloop)),
-        (r"/output/psd/narrowband", PSDHandler,
+
+    my_application = tornado.web.Application([
+        (r"/apps/(.*)/$", IndexHandler),
+        (r"/apps/(.*)", tornado.web.StaticFileHandler, {"path": static_dir}),
+        (_BASE_URL + r"/survey", SurveyHandler, dict(rtl_app=rtl_app, ioloop=_ioloop)),
+        (_BASE_URL + r"/device", DeviceHandler, dict(rtl_app=rtl_app, ioloop=_ioloop)),
+        (_BASE_URL + r"/status", EventHandler, dict(rtl_app=rtl_app, _ioloop=_ioloop)),
+        (_BASE_URL + r"/output/psd/narrowband", PSDHandler,
          dict(rtl_app=rtl_app, port_type=rtl_app.PORT_TYPE_NARROWBAND, _ioloop=_ioloop)),
-        (r"/output/psd/wideband", PSDHandler,
+        (_BASE_URL + r"/output/psd/wideband", PSDHandler,
          dict(rtl_app=rtl_app, port_type=rtl_app.PORT_TYPE_WIDEBAND, _ioloop=_ioloop)),
-        (r"/output/psd/fm", PSDHandler,
+        (_BASE_URL + r"/output/psd/fm", PSDHandler,
          dict(rtl_app=rtl_app, port_type=rtl_app.PORT_TYPE_FM, _ioloop=_ioloop))
     ], debug=debug)
 
-    return application
+    return my_application
 
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self, path):
+        index = os.path.join(static_dir, path, "index.html")
+        self.render(index)
 
 if __name__ == '__main__':
 
